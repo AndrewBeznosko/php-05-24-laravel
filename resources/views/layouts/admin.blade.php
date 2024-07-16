2a2cf22b1c1c8d2b99146258bbb4d8fa35526d83
AndrewBeznosko <andrew.beznosko@flexmade.com>
2024-07-16 19:05:34 +0300

    feat(hw-23): Laravel: Admin Categories CRUD

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -32,7 +32,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
                 <ul class="navbar-nav me-auto">
-
+                    @include('admin.navigation')
                 </ul>
 
                 <!-- Right Side Of Navbar -->
@@ -51,7 +51,29 @@
                             </li>
                         @endif
                     @else
-                        @include('admin.navigation')
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                               data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                @hasanyrole('moderator|admin')
+                                <a class="dropdown-item" href="{{ route('admin.dashboard') }}">
+                                    Admin Panel
+                                </a>
+                                @endhasanyrole
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
                     @endguest
                 </ul>
             </div>
